700bf8031114f1e0400b10cd991ecaabab5a1026
corei8 <corei8.github@gmail.com>
2026-01-15 14:34:59 -0500

    update port to match server

--- app.py
+++ app.py
@@ -210,4 +210,4 @@ with app.app_context():
     ensure_admin_user()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=3000)
